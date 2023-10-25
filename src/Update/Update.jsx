ce89359c13e9d9c6c5e1249da9220254e81f7111
kdrbipul <kdrbipul@gmail.com>
2023-10-26 02:02:25 +0600

    some correcton

--- src/Update/Update.jsx
+++ src/Update/Update.jsx
@@ -24,14 +24,14 @@ const Update = () => {
             <form onSubmit={handleUpdateUser} className='bg-warning m-5 p-5 rounded-2'>
             <div className="mb-3 text-start">
                 <label  className="form-label">Name</label>
-                <input onBlur={handleOnBlur} type="text" name="name" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required />
+                <input onBlur={handleOnBlur} type="text" name="name" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" defaultValue={storedUser.name} required />
             </div>
             <div className="mb-3 text-start">
                 <label  className="form-label">Email</label>
-                <input onBlur={handleOnBlur} type="email" name="email" className="form-control" id="exampleInputPassword1" required />
+                <input onBlur={handleOnBlur} type="email" name="email" className="form-control" id="exampleInputPassword1" defaultValue={storedUser.email} required />
             </div>
             
-            <button type="submit" className="btn btn-success">Submit</button>
+            <button type="submit" className="btn btn-success">Update User</button>
         </form>
             
         </div>
