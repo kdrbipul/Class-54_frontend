864f0ed0e2f8e9c82445d1b87b6e3dc81c7161ab
kdrbipul <kdrbipul@gmail.com>
2023-10-25 17:06:44 +0600

    form complete

--- src/Component/AddUser/AddUser.jsx
+++ src/Component/AddUser/AddUser.jsx
@@ -1,20 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const AddUser = () => {
+
+    const [user, setUser] = useState({});
+    console.log(user);
+
+
+    const handleAddUser = (e) =>{
+        e.preventDefault();
+        console.log("clicked the button");
+        const form = e.target;
+    }
+
+    const handleOnBlur = (e) =>{
+        e.preventDefault();
+        const field = e.target.name;
+        // console.log(field);
+        const value = e.target.value;
+        // console.log(value);
+        const newUser = {...user};
+        newUser [field] = value;
+        setUser(newUser)
+    }
     return (
         <>
-        <form>
-            <div class="mb-3">
-                <label  class="form-label">Email address</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+        <form onSubmit={handleAddUser} className='bg-warning m-5 p-5 rounded-2'>
+            <div className="mb-3 text-start">
+                <label  className="form-label">Name</label>
+                <input onBlur={handleOnBlur} type="text" name="name" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
             </div>
-            <div class="mb-3">
-                <label  class="form-label">Password</label>
-                <input type="password" class="form-control" id="exampleInputPassword1" />
+            <div className="mb-3 text-start">
+                <label  className="form-label">Email</label>
+                <input onBlur={handleOnBlur} type="email" name="email" className="form-control" id="exampleInputPassword1" />
             </div>
             
-            <button type="submit" class="btn btn-primary">Submit</button>
+            <button type="submit" className="btn btn-success">Submit</button>
         </form>
         </>
     );
